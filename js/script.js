3c8e59389fa1610d438bf6b9bc0c44068301923b
yqsmim <yasmimb.oliv@gmail.com>
2025-05-14 22:57:46 -0300

    atualizacao

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
   const width = 10;
   const heigth = 20;
   const size = width * heigth;
@@ -8,7 +8,7 @@ addEventListener("DOMContentLoaded", () => {
   const squares = Array.from(grid.querySelectorAll(".square"));
 
   function createGrid() {
-    let grid = document.querySelector("div");
+    let grid = document.querySelector(".grid");
     for (let i = 0; i < size; i++) {
       const square = document.createElement("div");
       square.classList.add("square");
@@ -18,7 +18,7 @@ addEventListener("DOMContentLoaded", () => {
   }
 
   const lTetromino = [
-    [1, width + 1, width * 2 + 1, 2], // ou seja, células: (0,1), (1,1), (2,1), (0,2)
+    [1, width + 1, width * 2 + 1, 2],
     [width, width + 1, width + 2, width * 2 + 2],
     [1, width + 1, width * 2 + 1, width * 2],
     [width, width * 2, width * 2 + 1, width * 2 + 2],
@@ -65,27 +65,125 @@ addEventListener("DOMContentLoaded", () => {
   let random = Math.floor(Math.random() * allTetrominoes.length);
   let current = allTetrominoes[random][currentRotation];
 
-  //draw the tretomino
   function draw() {
     current.forEach((index) => {
       squares[currentPosition + index].classList.add("tetromino");
     });
   }
 
-  //undraw the tetromino
   function undraw() {
     current.forEach((index) => {
       squares[currentPosition + index].classList.remove("tetromino");
     });
   }
 
-  timerID = setInterval(moveDown, 1000); // <- move down every second
+  //assign functions to keyCodes
+  function control(e) {
+    if (e.keyCode === 37) {
+      moveLeft();
+    }else if (e.keyCode === 39) {
+      moveRight();
+    }else if (e.keyCode === 38) {
+      rotate();
+  }else if (e.keyCode === 40) {
+      moveDown();
+    }
+  }
+  document.addEventListener("keyup", control);
 
   function moveDown() {
     undraw();
     currentPosition += width;
     draw();
+    freeze();
   }
+
+  function freeze() {
+    const reachedBottom = current.some(
+      (index) => currentPosition + index + width >= size
+    );
+
+    const nextRowTaken = current.some(
+      (index) =>
+        squares[currentPosition + index + width] &&
+        squares[currentPosition + index + width].classList.contains("taken")
+    );
+
+    if (reachedBottom || nextRowTaken) {
+      current.forEach((index) =>
+        squares[currentPosition + index].classList.add("taken")
+      );
+      random = Math.floor(Math.random() * allTetrominoes.length);
+      currentRotation = 0;
+      current = allTetrominoes[random][currentRotation];
+      currentPosition = 4;
+      draw();
+    }
+  }
+
+  //move tetromino left
+  function moveLeft() {
+    undraw();
+    const isAtLeftEdge = current.some(
+      (index) => (currentPosition + index) % width === 0
+    );
+    if (!isAtLeftEdge) currentPosition -= 1;
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition += 1;
+    }
+    draw();
+  }
+
+  //move tetromino right
+  function moveRight() {
+    undraw();
+    const isAtRightEdge = current.some(
+      (index) => (currentPosition + index) % width === width - 1
+    );
+    if (!isAtRightEdge) currentPosition += 1;
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition -= 1;
+    }
+    draw();
+  }
+
+  //rotate tetromino
+  function rotate() {
+    undraw();
+    currentRotation++;
+    if (currentRotation === current.length) {
+      currentRotation = 0;
+    }
+    current = allTetrominoes[random][currentRotation];
+    const isAtRightEdge = current.some(
+      (index) => (currentPosition + index) % width === width - 1
+    );
+    const isAtLeftEdge = current.some(
+      (index) => (currentPosition + index) % width === 0
+    );
+
+    if (isAtRightEdge && isAtLeftEdge) {
+      currentPosition -= 1;
+    }
+
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("taken")
+      )
+    ) {
+      currentPosition += 1;
+    }
+    draw();
+  }
+  
 
   setTimeout(() => {
     draw();
